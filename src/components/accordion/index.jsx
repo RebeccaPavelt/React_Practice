ae21b776a7ec601415cf78904798d5326cd9e225
Rebecca Pavelt <rebeccapavelt@gmx.de>
2025-03-12 16:20:09 +0100

    select_single_question

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,17 +1,34 @@
 import data from "./data";
+import { useState } from "react";
 
 export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
   return (
-    <div>
+    <div className="wrapper">
       <div>Accordion</div>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>
-            <strong>{item.question}</strong>
-            <p>{item.answer}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((item) => (
+            <div key={item.id}>
+              <div
+                className="title"
+                onClick={() => handleSingleSelection(item.id)}
+              >
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>{" "}
+              {selected === item.id ? <div>{item.answer}</div> : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
     </div>
   );
 }
