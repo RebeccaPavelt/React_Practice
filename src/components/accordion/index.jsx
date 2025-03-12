0792c9a006943c75e06f96946aaaf163316506bd
Rebecca Pavelt <rebeccapavelt@gmx.de>
2025-03-12 19:30:10 +0100

    implement_button

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -4,21 +4,42 @@ import "./styles.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelections, setEnableMultiSelections] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
 
+  function toggleButton() {
+    setEnableMultiSelections(!enableMultiSelections);
+  }
+
+  function handleMultiSelections(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    setMultiple(copyMultiple);
+  }
+
   return (
     <div className="wrapper">
       <h2>ACCORDION</h2>
+      <button type="button" className="button" onClick={toggleButton}>
+        Enable Multiple Selections
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((item) => (
             <div className="item" key={item.id}>
               <div
                 className="title"
-                onClick={() => handleSingleSelection(item.id)}
+                onClick={
+                  enableMultiSelections
+                    ? () => handleMultiSelections(item.id)
+                    : () => handleSingleSelection(item.id)
+                }
               >
                 <h3>{item.question}</h3>
                 <span className="span">+</span>
