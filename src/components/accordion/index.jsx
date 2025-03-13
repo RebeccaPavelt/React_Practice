6451e9d273a529cdd6ff3e1179b8f2ea35fce0e6
Rebecca Pavelt <rebeccapavelt@gmx.de>
2025-03-13 13:24:42 +0100

    add_multiple_selection_functionality

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
   const [enableMultiSelections, setEnableMultiSelections] = useState(false);
-  const [multiple, setMultiple] = useState([]);
+  const [openItems, setOpenItems] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
@@ -16,18 +16,20 @@ export default function Accordion() {
   }
 
   function handleMultiSelections(getCurrentId) {
-    let copyMultiple = [...multiple];
-    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
-
-    console.log(findIndexOfCurrentId);
-    setMultiple(copyMultiple);
+    setOpenItems((prevOpenItems) =>
+      prevOpenItems.includes(getCurrentId)
+        ? prevOpenItems.filter((id) => id !== getCurrentId)
+        : [...prevOpenItems, getCurrentId]
+    );
   }
 
   return (
     <div className="wrapper">
       <h2>ACCORDION</h2>
       <button type="button" className="button" onClick={toggleButton}>
-        Enable Multiple Selections
+        {enableMultiSelections
+          ? "Enable Single Selection"
+          : "Enable Multiple Selections"}
       </button>
       <div className="accordion">
         {data && data.length > 0 ? (
@@ -44,7 +46,13 @@ export default function Accordion() {
                 <h3>{item.question}</h3>
                 <span className="span">+</span>
               </div>{" "}
-              {selected === item.id ? <div>{item.answer}</div> : null}
+              {enableMultiSelections ? (
+                openItems.includes(item.id) ? (
+                  <div>{item.answer}</div>
+                ) : null
+              ) : selected === item.id ? (
+                <div>{item.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
